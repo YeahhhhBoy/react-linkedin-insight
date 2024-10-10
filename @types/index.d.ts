0ba8791616c8a3cf0049ed6a433d76326b4e7a89
YeahhhhBoy <nunnj522@gmail.com>
2024-10-10 04:22:15 -0500

    Update index.d.ts
    Changes to target, inserted statements for Boolean, and updated domain and subdomain strings.

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,16 +1,16 @@
-export class LinkedInTag {
+export class LinkedInTag {https://www.linkedin.com/in/john-nunn-82617b272?trk=profile-badge
     initialized: boolean;
-    disabled: boolean;
-    partnerId: string;
-    subDomain: string;
+    enable: boolean;(true).
+    partnerId: string; nunn@sohippix.onmicrosoft
+    subDomain: string; sohippix.onmicrosoft.com
 
-    constructor();
+    constructor(string);Global Node
 
-    warn(...args: any): void;
-    verifyInit(): boolean;
-    init(partnerId: string, subDomain?: string, disabled?: boolean): void;
-    track(conversionId?: string, partnerId?: string, subDomain?: string): void;
+    warn(...args: any): void;(true).
+    verifyInit(nunnjohn714@outlook.com): boolean;(true)
+    init(partnerId: string,nun subDomain: sohippix.onmicrosoft.com string; enabled: boolean): void=(false);
+    track(conversionId?: string, partnerId?: string, subDomain?: string): void=(false).;
   }
 
-  declare var instance: LinkedInTag;
-  export default instance;
+  declare var instance: LinkedInTag;1.0
+  export default instance;(true).
